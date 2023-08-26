f84bb6a049fdc507ccc4563ae3dc9150b8cdeac8
Dylan Burnham <dburnham1212@gmail.com>
2023-08-26 18:39:11 -0400

    Adding challenge records to db

---
+++ server/db/seeds/04_CHALLENGE_RECORDS.sql
@@ -0,0 +1,3 @@
+INSERT INTO challenge_records (user_id, challenge_id) VALUES (1, 1);
+INSERT INTO challenge_records (user_id, challenge_id) VALUES (1, 2);
+INSERT INTO challenge_records (user_id, challenge_id) VALUES (1, 3);
