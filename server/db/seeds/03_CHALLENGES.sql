0ea03ca357e944c5be7f11869b05bcbba01ad122
Dylan Burnham <dburnham1212@gmail.com>
2023-09-04 20:33:31 -0400

    Adding challenges for ollies

--- server/db/seeds/03_CHALLENGES.sql
+++ server/db/seeds/03_CHALLENGES.sql
@@ -1,4 +1,11 @@
 -- CHALLENGES FOR MANUALS
-INSERT INTO challenges (trick_id, title, description, exp_val) VALUES (1, 'Lean Back', 'Press down on the tail of your board and manual for a foot or two. Don''t try to push it and make sure you are confortable before trying longer distances.', 150);
-INSERT INTO challenges (trick_id, title, description, exp_val) VALUES (1, 'A Few Feet', 'Manual for 5 or more feet.', 150);
-INSERT INTO challenges (trick_id, title, description, exp_val) VALUES (1, 'A Few More', 'Manual for 10 or more feet.', 200);
+INSERT INTO challenges (trick_id, title, description, exp_val) VALUES 
+(1, 'Lean Back', 'Press down on the tail of your board and manual for a foot or two. Don''t try to push it and make sure you are confortable before trying longer distances.', 150),
+(1, 'A Few Feet', 'Manual for 5 or more feet.', 150),
+(1, 'A Few More', 'Manual for 10 or more feet.', 200);
+
+-- CHALLENGES FOR OLLIES
+INSERT INTO challenges (trick_id, title, description, exp_val) VALUES 
+(2, 'Getting Air', 'Get off of the ground with an ollie.', 200),
+(2, 'Down The Drop', 'Find a drop (it should be no more than 6 inches tall), and ollie down it.', 200),
+(2, 'Up The Curb', 'Find a curb (it should be no more than 6 inches tall), and ollie up it.', 200);
